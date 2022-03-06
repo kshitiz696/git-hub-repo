1a47cdd956948dcbd2a63815e080d63e6c0e9c56
kshitiz696 <75476689+kshitiz696@users.noreply.github.com>
2022-03-06 09:22:28 +0530

    adding changes on github to math.py

--- math.py
+++ math.py
@@ -1,10 +1,9 @@
 
 #Add the implementation
 from sre_constants import BRANCH
-
-
 def add(x,y)
 def sub(x,y)
 #Multiply implementation
 def mul(x,y)
 this is Bug12 branch
+I have donr some changes on github.
